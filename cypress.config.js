feb21638403c95a6ed8dd36d6017262ad9b97cde
Kaique Fernandes <kaique.1996@live.com>
2025-06-10 12:18:21 -0300

    alteração no config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,17 @@
 const { defineConfig } = require("cypress");
 
-        
-
 module.exports = defineConfig({
   reporter: 'mocha-junit-reporter',
   reporterOptions: {
-  mochaFile: 'cypress/results/results-[hash].xml'
+    mochaFile: 'cypress/results/results-[hash].xml',
   },
-
   e2e: {
-                setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {
+      
     },
+    baseUrl: 'https://opensource-demo.orangehrmlive.com', 
+    defaultCommandTimeout: 10000, 
+    pageLoadTimeout: 30000, 
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
