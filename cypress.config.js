8ac471126057aa8e981e7b9147594b640f43e52d
Kaique Fernandes <kaique.1996@live.com>
2025-06-10 17:19:37 -0300

    Testando nova versao do arquivo config.js

--- cypress.config.js
+++ cypress.config.js
@@ -13,5 +13,6 @@ module.exports = defineConfig({
     defaultCommandTimeout: 20000, 
     pageLoadTimeout: 50000, 
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
   },
 });
